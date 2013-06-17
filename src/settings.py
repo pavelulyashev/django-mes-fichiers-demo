3840aae826eb0e349d7144280ef84a343547936d
Pavel Ulyashev <p.ulyashev@gmail.com>
2013-06-17 10:00:59 +0700

    Configure for appfog

--- src/settings.py
+++ src/settings.py
@@ -18,8 +18,8 @@ MANAGERS = ADMINS
 if 'VCAP_SERVICES' in os.environ:
     import json
 
-
     vcap_services = json.loads(os.environ['VCAP_SERVICES'])
+    print vcap_services
     # XXX: avoid hardcoding here
     mysql_srv = vcap_services['mysql-5.1'][0]
     cred = mysql_srv['credentials']
@@ -29,7 +29,7 @@ if 'VCAP_SERVICES' in os.environ:
             'NAME': cred['name'],
             'USER': cred['user'],
             'PASSWORD': cred['password'],
-            'HOST': cred['hostname'],
+            'HOST': cred['host'],
             'PORT': cred['port'],
         }
     }
@@ -161,29 +161,6 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
 
 TINYMCE_DEFAULT_CONFIG = {
     'plugins': [
